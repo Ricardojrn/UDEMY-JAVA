725de9e25e88a42eb53c330a1f646606ee48f3d0
Ricardojrn <cadonascimentosilva@hotmail.com>
2025-04-15 20:12:13 -0300

    resolvendo exercicio

--- aula82-exercicios/src/main/java/org/example/Conta.java
+++ aula82-exercicios/src/main/java/org/example/Conta.java
@@ -1,4 +1,48 @@
 package org.example;
 
 public class Conta {
+    private int conta;
+    private String titular;
+    private double saldo = 0.0;
+
+    public Conta(String titular, int conta, double depositoInicial) {
+        this.conta = conta;
+        this.titular = titular;
+        deposita(depositoInicial);
+    }
+
+    public Conta(String titular, int conta) {
+        this.conta = conta;
+        this.titular = titular;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void deposita(double valor){
+        saldo += valor;
+    }
+    public void saque(double valor){
+        saldo -= valor + 5;
+    }
+
+    @Override
+    public String toString() {
+        return "Número da conta: " + conta +
+                "\ntitular: " + titular +
+                "\nSaldo: " + saldo;
+    }
 }
--- aula82-exercicios/src/main/java/org/example/Main.java
+++ aula82-exercicios/src/main/java/org/example/Main.java
@@ -1,6 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ENTRE COM OS DADOS BANCÁRIOS PARA CRIAÇÃO DA NOVA CONTA");
+        System.out.print("Conta: ");
+        int conta = input.nextInt();
+        System.out.print("Nome do titular: ");
+        String titular = input.next();
+        System.out.println("Deseja fazer deposíto inicial?\n1-sim\n2-não");
+        int simOuNao = input.nextInt();
+        if (simOuNao == 1){
+            System.out.print("Entre com o valor do depósito: ");
+            double saldo = input.nextDouble();
+            Conta conta1 = new Conta(titular,conta,saldo);
+            System.out.println(conta1);
+        }else if (simOuNao == 2){
+            Conta conta1 = new Conta(titular,conta);
+            System.out.println(conta1);
+        }
+
+        System.out.print("Entre com um valor para o depósito: ");
+        double deposito = input.nextDouble();
+
     }
 }

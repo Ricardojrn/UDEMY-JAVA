cba823fa805323989a890867202644fc8616b6e7
Ricardojrn <cadonascimentosilva@hotmail.com>
2025-06-02 15:54:46 -0300

    Vetore - Array

---
+++ aula89-vetores/src/main/java/org/example/Program.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner  scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n ; i++) {
+            vect[i] = scan.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i=0; i<n; i++){
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("Altura média: %.2f%n",avg);
+
+        scan.close();
+    }
+}
